03f50eef950714a64865f7493be8009bd23d34b2
RASHMI KIRAN <147822933+RashmiKi@users.noreply.github.com>
2025-07-21 16:01:28 +0530

    Toothless done the correction

--- Function/S3.py
+++ Function/S3.py
@@ -1,6 +1,6 @@
 def multiply (data1, data2):
     multi = data1*data2
-    return(multi)
+    return multi
 
 d1 = int(input("Enter a number "))
 d2 = int(input("Enter a number "))
@@ -8,7 +8,7 @@ d2 = int(input("Enter a number "))
 print(multiply(d1,d2))
 
 
-dd1 = (input("Enter a string "))
+dd1 =int(input("Enter a number "))
 dd2 = (input("Enter a string "))
 
 print(multiply(dd1,dd2))
